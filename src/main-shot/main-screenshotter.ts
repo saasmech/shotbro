83b6012e7436f9ba4c154b3310705bc1d1cda710
Paul Tuckey <paul@tuckey.org>
2022-09-17 13:01:14 +1200

    feat: use root html

--- src/main-shot/main-screenshotter.ts
+++ src/main-shot/main-screenshotter.ts
@@ -2,19 +2,20 @@ import type {Page} from "playwright";
 import * as fs from "fs";
 
 const MAIN_CAPTURE_HEIGHT_LIMIT = 4000;
+const MAIN_CAPTURE_WIDTH_LIMIT = 4000;
 
 export async function generateMainScreenshot(page: Page, htmlContentPath: string, screenshotPath: string) {
-  const body  = await page.locator('body');
-  const bodyBb = await body.boundingBox()
-  if (!bodyBb) throw new Error('Could not get size of HTML body');
+  const rootEl  = await page.locator('html');
+  const rootBb = await rootEl.boundingBox()
+  if (!rootBb) throw new Error('Could not get size of HTML element');
   await page.screenshot({
     path: screenshotPath,
     fullPage: true,
     animations: 'disabled',
     clip: {
-      x: bodyBb.x, y: bodyBb.y,
-      width: bodyBb.width, // todo: future, handle very wide also
-      height: bodyBb.height > 4000 ? MAIN_CAPTURE_HEIGHT_LIMIT: bodyBb.height
+      x: rootBb.x, y: rootBb.y,
+      width: rootBb.width > MAIN_CAPTURE_WIDTH_LIMIT ? MAIN_CAPTURE_WIDTH_LIMIT: rootBb.width,
+      height: rootBb.height > MAIN_CAPTURE_HEIGHT_LIMIT ? MAIN_CAPTURE_HEIGHT_LIMIT: rootBb.height,
     }
   });
 
@@ -25,14 +26,16 @@ export async function generateMainScreenshot(page: Page, htmlContentPath: string
     // sugges: optionally ensure the page is scrolled to the top
     // window.scroll({top: 0, left: 0, behavior: 'auto'});
 
-    const addSbBb = async function (el: Element) {
+    const addSbBb = async function (el: Element|null) {
       // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+      if (!el) return;
       const bb = await el.getBoundingClientRect();
       // data-sb-bb = Shot Bro Bounding Box / in css style key:value;...
       el.setAttribute('data-sb-bb', `x:${bb.x};y:${bb.y};w:${bb.width};h:${bb.height};`)
     };
-    await addSbBb(document.body);
     document.body.querySelectorAll('*').forEach(addSbBb);
+    await addSbBb(document.body);
+    await addSbBb(document.querySelector('html'));
   });
   const content = await page.content();
   fs.writeFileSync(htmlContentPath, content);
